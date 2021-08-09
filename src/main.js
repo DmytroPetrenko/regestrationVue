a9593ab8fd355a35d64b0504ad84f8b73a6e2108
Dmytro Petrenko <dmitry.a.petrenko@gmail.com>
2021-08-09 14:35:02 +0300

    ui done

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
-import Vue from "vue";
-import App from "./App.vue";
-import store from "./store";
+import Vue from "vue"
+import App from "./App.vue"
+import VueRouter from "vue-router"
+import store from "./store"
+import vuetify from "./plugins/vuetify"
+import RegestrationForm from "@/components/RegestrationForm"
+import UserPage from "@/components/UserPage"
 
-Vue.config.productionTip = false;
+Vue.use(VueRouter)
+
+const routes = [
+	{
+		path: "",
+		component: RegestrationForm,
+	},
+	{
+		path: "/user",
+		component: UserPage,
+	},
+]
+
+const router = new VueRouter({
+	routes,
+	mode: "history",
+})
+
+Vue.config.productionTip = false
 
 new Vue({
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+	store,
+	router,
+	vuetify,
+	render: (h) => h(App),
+}).$mount("#app")
